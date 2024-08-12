4167b76107e7d213ce29f0ff103360858a16d997
haizhong lin <771952870@qq.com>
2024-08-13 02:43:48 +0930

    Submit weekly and add unit test cases

--- Haizhong_s363429/Week1/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Haizhong_s363429/Week1/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee617cc8f8b41e81136ebcdb4b25bdc3e357e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2374bbf6321f96de029110879ecb4ca52993b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Haizhong_s363429/week3/MyUnitTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestApi(unittest.TestCase):
+    def test_1(self):
+        print("Hello world")
+    
+    def test_2(self):
+        print("HAPPY TIME")
+
+if __name__ == '__main__':
+    unittest.main()
